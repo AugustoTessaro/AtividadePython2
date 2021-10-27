99f467d86c0ca958a25784a86b70c02041333a46
AugustoTessaro <atessaro15@gmail.com>
2021-10-27 09:47:49 -0300

    Update Controle_Disco.py

--- Controle_Disco.py
+++ Controle_Disco.py
@@ -1,6 +1,27 @@
+from os import spawnl
 import pandas as pd
-import twilio
+import twilio as tw
 
+'''
 with open('Usuarios.txt', 'r') as arquivo:
     for posicao, valores in (enumerate(arquivo)):
         print(posicao + 1, valores)
+'''
+
+usuarios = []
+armaz_usuarios = []
+
+tabela = pd.read_table('Usuarios.txt', header= None, sep=" ")
+
+usuarios = tabela[0]
+armaz_usuarios = tabela[1]
+
+print(usuarios,armaz_usuarios)
+
+
+
+
+
+
+
+
